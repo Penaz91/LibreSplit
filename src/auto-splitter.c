1037b3b59f344eb96eca24621e637d21e7cb2aeb
Penaz <penazarea@altervista.org>
2025-11-30 11:50:35 +0100

    fix(auto-splitter): Fix wrong ordering of pushing functions to Lua

--- src/auto-splitter.c
+++ src/auto-splitter.c
@@ -352,9 +352,9 @@ void run_auto_splitter()
     lua_pushcfunction(L, get_base_address);
     lua_setglobal(L, "getBaseAddress");
     lua_pushcfunction(L, read_address);
-    lua_setglobal(L, "sizeOf");
-    lua_pushcfunction(L, size_of);
     lua_setglobal(L, "readAddress");
+    lua_pushcfunction(L, size_of);
+    lua_setglobal(L, "sizeOf");
     lua_pushcfunction(L, perform_sig_scan);
     lua_setglobal(L, "sig_scan");
     lua_pushcfunction(L, getPid);
