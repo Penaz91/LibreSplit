f5177296bfb7a66a32ec99a707f1a906d647de22
Penaz <penazarea@altervista.org>
2025-12-08 09:58:14 +0100

    Fixed wrong comparison for cache

--- src/process.c
+++ src/process.c
@@ -84,7 +84,7 @@ uint64_t get_module_size(const char* module)
     // Check the cache
     for (int32_t i = 0; i < p_maps_cache_size; i++) {
         const char* name = p_maps_cache[i].name;
-        if (strstr(name, module_to_grep) == NULL) {
+        if (strstr(name, module_to_grep) != NULL) {
             return p_maps_cache[i].size;
         }
     }
