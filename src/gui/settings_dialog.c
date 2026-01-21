8c5c2089cfbc80c961c26d753e1752dbb03a3069
Penaz <penazarea@altervista.org>
2026-01-21 20:10:00 +0100

    Try to tie the actual settings to the GTK widgets

--- src/gui/settings_dialog.c
+++ src/gui/settings_dialog.c
@@ -4,8 +4,27 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
+LSGuiSetting* gui_settings;
+int settings_number = 0;
+
+static int enumerate_settings(AppConfig cfg)
+{
+    for (size_t s = 0; s < sections_count; ++s) {
+        SectionInfo section_info = sections[s];
+        if (!section_info.in_gui) {
+            continue;
+        }
+        settings_number += section_info.count;
+    }
+    return settings_number;
+}
+
 static void build_settings_dialog(GtkApplication* app, gpointer data)
 {
+    int settings_number = enumerate_settings(cfg);
+    // TODO: [Penaz] [2026-01-21] FREE MEMORY AT THE CLOSE OF WIN
+    gui_settings = malloc(settings_number * sizeof(LSGuiSetting));
+
     GtkWidget* window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "LibreSplit Settings");
     gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
@@ -17,6 +36,7 @@ static void build_settings_dialog(GtkApplication* app, gpointer data)
     gtk_widget_set_margin_end(tabs, 8);
     gtk_widget_set_vexpand(tabs, TRUE);
     gtk_widget_set_hexpand(tabs, TRUE);
+    int settings_idx = 0;
     for (size_t s = 0; s < sections_count; ++s) {
         SectionInfo section_info = sections[s];
         if (!section_info.in_gui) {
@@ -32,36 +52,41 @@ static void build_settings_dialog(GtkApplication* app, gpointer data)
         GtkWidget* title = gtk_accel_label_new(section_info.name);
         for (size_t i = 0; i < section_info.count; ++i) {
             ConfigEntry entry = ((ConfigEntry*)section_info.entries)[i];
+            gui_settings[settings_idx].settings_entry = &entry;
             switch (entry.type) {
                 case CFG_STRING:
                     GtkWidget* lbl_str = gtk_label_new(entry.desc);
                     gtk_container_add(GTK_CONTAINER(box), lbl_str);
-                    GtkEntryBuffer* entry_box_buffer_str = gtk_entry_buffer_new(entry.value.s, sizeof(entry.value.s));
-                    GtkWidget* entry_box_str = gtk_entry_new_with_buffer(entry_box_buffer_str);
-                    gtk_container_add(GTK_CONTAINER(box), entry_box_str);
+
+                    gui_settings[settings_idx].entry_buffer = gtk_entry_buffer_new(entry.value.s, sizeof(entry.value.s));
+                    gui_settings[settings_idx].widget = gtk_entry_new_with_buffer(gui_settings[settings_idx].entry_buffer);
+                    gtk_container_add(GTK_CONTAINER(box), gui_settings[settings_idx].widget);
                     break;
                 case CFG_KEYBIND:
                     GtkWidget* lbl_kb = gtk_label_new(entry.desc);
                     gtk_container_add(GTK_CONTAINER(box), lbl_kb);
-                    GtkEntryBuffer* entry_box_buffer_kb = gtk_entry_buffer_new(entry.value.s, sizeof(entry.value.s));
-                    GtkWidget* entry_box_kb = gtk_entry_new_with_buffer(entry_box_buffer_kb);
-                    gtk_container_add(GTK_CONTAINER(box), entry_box_kb);
+
+                    gui_settings[settings_idx].entry_buffer = gtk_entry_buffer_new(entry.value.s, sizeof(entry.value.s));
+                    gui_settings[settings_idx].widget = gtk_entry_new_with_buffer(gui_settings[settings_idx].entry_buffer);
+                    gtk_container_add(GTK_CONTAINER(box), gui_settings[settings_idx].widget);
                     break;
                 case CFG_BOOL:
-                    GtkWidget* checkbox = gtk_check_button_new_with_label(entry.desc);
-                    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(checkbox), entry.value.b);
-                    gtk_container_add(GTK_CONTAINER(box), checkbox);
+                    gui_settings[settings_idx].widget = gtk_check_button_new_with_label(entry.desc);
+                    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(gui_settings[settings_idx].widget), entry.value.b);
+                    gtk_container_add(GTK_CONTAINER(box), gui_settings[settings_idx].widget);
                     break;
                 case CFG_INT:
                     GtkWidget* lbl_int = gtk_label_new(entry.desc);
                     gtk_container_add(GTK_CONTAINER(box), lbl_int);
                     char setting_as_str[(int)((entry.value.i / 10) + 1)];
                     sprintf(setting_as_str, "%d", entry.value.i);
-                    GtkEntryBuffer* entry_box_buffer_int = gtk_entry_buffer_new(setting_as_str, sizeof(setting_as_str));
-                    GtkWidget* entry_box_int = gtk_entry_new_with_buffer(entry_box_buffer_int);
-                    gtk_container_add(GTK_CONTAINER(box), entry_box_int);
+
+                    gui_settings[settings_idx].entry_buffer = gtk_entry_buffer_new(setting_as_str, sizeof(setting_as_str));
+                    gui_settings[settings_idx].widget = gtk_entry_new_with_buffer(gui_settings[settings_idx].entry_buffer);
+                    gtk_container_add(GTK_CONTAINER(box), gui_settings[settings_idx].widget);
                     break;
             }
+            settings_idx++;
         }
         gtk_notebook_append_page(GTK_NOTEBOOK(tabs), box, title);
     }
--- src/gui/settings_dialog.h
+++ src/gui/settings_dialog.h
@@ -5,4 +5,10 @@
 
 extern AppConfig cfg;
 
+typedef struct LSGuiSetting {
+    GtkWidget* widget;
+    GtkEntryBuffer* entry_buffer;
+    ConfigEntry* settings_entry;
+} LSGuiSetting;
+
 void show_settings_dialog(GSimpleAction* action, GVariant* parameter, gpointer app);
