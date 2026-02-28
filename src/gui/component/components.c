3a5128b5cb8e314e1ad7b0041ffe053fca22fb12
Penaz <penazarea@altervista.org>
2026-02-28 20:34:41 +0100

    Fix segfaulting
    Apparently I have an IQ below body temperature.

--- src/gui/component/components.c
+++ src/gui/component/components.c
@@ -29,7 +29,7 @@ LSComponentAvailable ls_available_components[] = {
 
 LSComponentAvailable* ls_components;
 
-void initialize_components()
+void initialize_components(void)
 {
     SectionInfo* section = NULL;
     for (size_t i = 0; i < sections_count; i++) {
@@ -43,13 +43,13 @@ void initialize_components()
         abort();
     }
     int components_count = 0;
-    for (size_t i = 0; i < sizeof(section->count); i++) {
+    for (size_t i = 0; i < section->count; i++) {
         ConfigEntry entry = ((ConfigEntry*)section->entries)[i];
         if (entry.value.b) {
             components_count++;
         }
     }
-    ls_components = malloc(components_count + 1 * sizeof(LSComponentAvailable));
+    ls_components = malloc((components_count + 1) * sizeof(LSComponentAvailable));
     if (!ls_components) {
         printf("Cannot allocate memory for components");
         abort();
