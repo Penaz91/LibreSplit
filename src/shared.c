d29366bacba71589b7f4658cf0fb5921e1af1b0c
Penaz <penazarea@altervista.org>
2026-03-11 18:13:08 +0100

    Added logging to `shared.c`

--- src/shared.c
+++ src/shared.c
@@ -1,4 +1,5 @@
 #include "shared.h"
+#include "logging.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,14 +16,17 @@
  */
 void getXDGruntimeDir(char* buffer, size_t size)
 {
+    LOG_DEBUG("Searching for XDG_RUNTIME_DIR");
     const char* xdg_runtime_dir = getenv("XDG_RUNTIME_DIR");
     buffer[0] = '\0';
     if (xdg_runtime_dir) {
+        LOG_DEBUGF("XDG_RUNTIME_DIR found at %s", xdg_runtime_dir);
         strncpy(buffer, xdg_runtime_dir, size - 1);
         buffer[size - 1] = '\0';
         return;
     }
 
     const int uid = getuid();
+    LOG_DEBUGF("XDG_RUNTIME_DIR not found, defaulting to /run/user/%s", uid);
     snprintf(buffer, size, "/run/user/%d", uid);
 }
