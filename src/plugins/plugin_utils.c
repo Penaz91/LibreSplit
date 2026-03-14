402026b7469895bf77edf22aa8c199d34626732a
Penaz <penazarea@altervista.org>
2026-03-14 21:53:01 +0100

    Added utilities for adding hooks into timer events

--- src/plugins/plugin_utils.c
+++ src/plugins/plugin_utils.c
@@ -1,6 +1,7 @@
 #include "plugin_utils.h"
 #include "lua.h"
 #include "src/logging.h"
+#include "src/timer.h"
 #include <stdlib.h>
 #include <string.h>
 
@@ -42,6 +43,32 @@ int register_lua_function(const char* name, lua_CFunction fn)
 }
 
 /**
+ * Utility function to push a function into the right registry.
+ *
+ * @param registry The registry to register the function on
+ * @param fn The function to register
+ *
+ * @returns 0 if everything went well. An error code otherwise.
+ */
+static int push_function(TimerHookRegistry* registry, timer_hook_func fn)
+{
+    LOG_DEBUG("Pushing new function to hooks array");
+    if (registry->count == registry->size) {
+        LOG_DEBUG("Reallocating array for size");
+        // Resize array
+        registry->size *= 2;
+        registry->functions = realloc(registry->functions, registry->size * sizeof(timer_hook_func));
+        if (!registry->functions) {
+            LOG_ERR("Cannot reallocate memory for timer hook.");
+            abort();
+        }
+    }
+    registry->functions[registry->count] = fn;
+    registry->count++;
+    return 0;
+}
+
+/**
  * Register a plugin function to be called when a certain event happens.o
  *
  * @param[in] event The event to react to.
@@ -49,39 +76,51 @@ int register_lua_function(const char* name, lua_CFunction fn)
  *
  * @returns 0 if everything went okay, otherwise an error code.
  */
-int register_event_hook(HookableEvent event, void* fn)
+int register_event_hook(HookableEvent event, timer_hook_func fn)
 {
     // TODO: [Penaz] [2026-03-13] STUB
     switch (event) {
         case START:
-            LOG_ERR("Hooking to start event not implemented");
+            LOG_DEBUG("Hooking new function into start event");
+            push_function(&start_hooks, fn);
             break;
         case SPLIT:
-            LOG_ERR("Hooking to split event not implemented");
+            LOG_DEBUG("Hooking new function into split event");
+            push_function(&split_hooks, fn);
             break;
         case STOP:
-            LOG_ERR("Hooking to stop event not implemented");
+            LOG_DEBUG("Hooking new function into stop event");
+            push_function(&stop_hooks, fn);
             break;
         case RESET:
-            LOG_ERR("Hooking to reset event not implemented");
+            LOG_DEBUG("Hooking new function into reset event");
+            push_function(&reset_hooks, fn);
             break;
         case CANCEL:
-            LOG_ERR("Hooking to cancel event not implemented");
+            LOG_DEBUG("Hooking new function into cancel event");
+            push_function(&cancel_hooks, fn);
             break;
         case SKIP:
-            LOG_ERR("Hooking to skip event not implemented");
+            LOG_DEBUG("Hooking new function into skip event");
+            push_function(&skip_hooks, fn);
             break;
         case UNSPLIT:
-            LOG_ERR("Hooking to unsplit event not implemented");
+            LOG_DEBUG("Hooking new function into unsplit event");
+            push_function(&unsplit_hooks, fn);
             break;
         case PAUSE:
-            LOG_ERR("Hooking to pause event not implemented");
+            LOG_DEBUG("Hooking new function into pause event");
+            push_function(&pause_hooks, fn);
             break;
         case UNPAUSE:
-            LOG_ERR("Hooking to unpause event not implemented");
+            LOG_DEBUG("Hooking new function into unpause event");
+            push_function(&start_hooks, fn);
             break;
+        default:
+            LOG_WARN("Tried to hook to nonexisting event, ignored.");
+            return -1;
     }
-    return -1;
+    return 0;
 }
 
 void init_external_lasr_functions(void)
--- src/plugins/plugin_utils.h
+++ src/plugins/plugin_utils.h
@@ -1,8 +1,19 @@
 #pragma once
 #include "../lasr/auto-splitter.h"
+#include "../timer.h"
 #include "lua.h"
 
 extern ExternalLASRFunctionRegistry external_lasr_functions;
+
+extern TimerHookRegistry start_hooks;
+extern TimerHookRegistry stop_hooks;
+extern TimerHookRegistry split_hooks;
+extern TimerHookRegistry reset_hooks;
+extern TimerHookRegistry cancel_hooks;
+extern TimerHookRegistry skip_hooks;
+extern TimerHookRegistry unsplit_hooks;
+extern TimerHookRegistry pause_hooks;
+extern TimerHookRegistry unpause_hooks;
 
 /*! \enum event
  *
@@ -28,6 +39,6 @@ typedef struct PlugAPI {
 
 int register_lua_function(const char* name, lua_CFunction);
 
-int register_event_hook(HookableEvent event, void* fn);
+int register_event_hook(HookableEvent event, timer_hook_func fn);
 
 void init_external_lasr_functions(void);
