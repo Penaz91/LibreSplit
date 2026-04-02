2daa3531ca4f60f44d329faa03a3dc922dd8ca55
Penaz <penazarea@altervista.org>
2026-04-02 14:43:01 +0200

    Trying to make the memory iterator a bit more solid

--- src/lasr/memory_iter/memory_iterator.c
+++ src/lasr/memory_iter/memory_iterator.c
@@ -49,29 +49,34 @@ MemoryIterator* mem_iterator_new(pid_t pid, uintptr_t start, uintptr_t end, uint
 int mem_next(uint8_t** buffer, size_t* buffer_size, MemoryIterator* iterator, uint8_t* err)
 {
     assert(iterator != NULL);
+    assert(iterator->cursor >= iterator->start);
     size_t window_size = MEMORY_WINDOW_SIZE;
     bool last_iter = false;
-    assert(iterator->cursor >= iterator->start);
     if (iterator->cursor >= iterator->end) {
         // We passed the end in the last iteration. Stop.
         return 0;
     }
     if (iterator->cursor + window_size > iterator->end) {
         // We're at the last iteration and the window size is too big
-        // FIXME: [Penaz] [2026-04-02] I received a short memory read while testing, this
-        // ^ might not work
         window_size = (size_t)(iterator->end - iterator->cursor);
         last_iter = true;
+        if (window_size == 0) {
+            // Nothing more to read
+            return 0;
+        }
     }
     // Reallocate buffer for reading
-    *buffer = (uint8_t*)realloc(*buffer, window_size);
-    if (*buffer == NULL) {
+    // PERF: Since we now accept short reads, we may not need to realloc every time
+    // ^ just alloc the biggest accepted read once. Maybe pointer should be in the iterator struct?
+    uint8_t* tmp = realloc(*buffer, window_size);
+    if (tmp == NULL) {
         // Cannot allocate buffer, stop immediately
         LOG_WARN("Unable to allocate memory read buffer");
         *err = 1;
         return 0;
     }
-    *buffer_size = window_size;
+    *buffer = tmp;
+    tmp = NULL;
     // Read memory
     struct iovec local_iov = { *buffer, window_size };
     struct iovec remote_iov = { (void*)iterator->cursor, window_size };
@@ -85,17 +90,43 @@ int mem_next(uint8_t** buffer, size_t* buffer_size, MemoryIterator* iterator, ui
             // Matches between chunks
             iterator->cursor -= iterator->overlap;
         }
+        *buffer_size = window_size;
         // There are more things to read
         return 1;
     } else {
         if (nread < 0) {
+            if (errno == EINTR) {
+                // TODO: [Penaz] [2026-04-02] Memory read interrupted by a transitory interruption.
+                // For now we'll still bail out. but ideally we should retry.
+                LOG_DEBUG("Read interrupted by EINTR");
+            }
             LOG_DEBUGF("Memory read error, errno is %d (%s)", errno, strerror(errno));
             LOG_DEBUGF("Cursor: %x, Start: %x, End: %x, Overlap: %x", iterator->cursor, iterator->start, iterator->end, iterator->overlap);
             *err = 3;
+        } else if (nread == 0) {
+            // No bytes read. We stop here
+            *buffer_size = 0;
+            return 0;
         } else {
-            // Memory read error
-            LOG_WARNF("Memory read error: nread=%zd, expected=%td", nread, window_size);
-            *err = 2;
+            // Short read
+            LOG_WARNF("Short memory read: nread=%zd, expected=%td", nread, window_size);
+            // Save the short read cursor
+            iterator->last_cursor = iterator->cursor;
+            // Advance by the number of bytes read
+            *buffer_size = (size_t)nread;
+            iterator->cursor += (size_t)nread;
+            if (!last_iter) {
+                // If we're not done, move it backwards a little to account for
+                // Matches between chunks. If the nread is shorter, we accept the lost loop
+                // and hope for next loop to come and save us
+                if (nread > iterator->overlap) {
+                    iterator->cursor -= iterator->overlap;
+                } else {
+                    // At least advance 1 byte
+                    iterator->cursor -= nread + 1;
+                }
+            }
+            return 1;
         }
     }
     // Nothing more to read or error has happened.
